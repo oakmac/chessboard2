27a921c7e18a15407ffc47ad183c72f0acaa73ca
Chris Oakman <chris@oakmac.com>
2022-10-05 01:51:46 -0500

    add broken test

--- cypress/e2e/example3001.cy.js
+++ cypress/e2e/example3001.cy.js
@@ -1,6 +1,11 @@
 describe('Example 3001', () => {
   beforeEach(() => {
     cy.visit('http://localhost:3232/examples/3001.html')
+
+    cy.get('#myBoard')
+      .should('have.length', 1)
+      .get('#myBoard .piece-349f8')
+      .should('have.length', 0)
   })
 
   it('Empty Board', () => {
@@ -8,15 +13,39 @@ describe('Example 3001', () => {
   })
 
   it('Start Position', () => {
-    cy.get('#setStartBtn').click()
-    cy.get('#myBoard .piece-349f8').should('have.length', 32)
+    cy.get('#setStartBtn')
+      .click()
+      .get('#myBoard .piece-349f8')
+      .should('have.length', 32)
   })
 
-  it('Start Position --> Rook Checkmate', () => {
-    cy.get('#setStartBtn').click()
-    cy.get('#myBoard .piece-349f8').should('have.length', 32)
+  it('Start Position --> Rook Checkmate --> Start Position', () => {
+    cy.get('#setStartBtn')
+      .click()
+      .get('#myBoard .piece-349f8').should('have.length', 32)
+      .get('#setRookCheckmateBtn').click()
+      .get('#myBoard .piece-349f8').should('have.length', 3)
+      .get('#setStartBtn').click()
+      .get('#myBoard .piece-349f8').should('have.length', 32)
+  })
 
-    cy.get('#setRookCheckmateBtn').click()
-    cy.get('#myBoard .piece-349f8').should('have.length', 3)
+  it('Start Position --> Pawn Attack', () => {
+    cy.get('#setStartBtn')
+      .click()
+      .get('#myBoard .piece-349f8').should('have.length', 32)
+      .get('#pawnAttackBtn').click()
+      .get('#myBoard .piece-349f8').should('have.length', 64)
+      .get('#pawnAttackBtn').click()
+      .get('#myBoard .piece-349f8').should('have.length', 64)
+      .get('#pawnAttackBtn').click()
+      .get('#myBoard .piece-349f8').should('have.length', 64)
+      .get('#pawnAttackBtn').click()
+      .get('#myBoard .piece-349f8').should('have.length', 64)
+      .get('#setStartBtn').click()
+      .get('#myBoard .piece-349f8').should('have.length', 32)
+
+      // FIXME: this does not work!
+      .get('#clearBoardBtn').click()
+      .get('#myBoard .piece-349f8').should('have.length', 0)
   })
 })
