bf1f370ed0d0a3db0ad0644c0693e805a0083205
Chris Oakman <chris@oakmac.com>
2022-10-20 02:15:28 -0500

    saving state

--- src-cljs/com/oakmac/chessboard2/util/dom.cljs
+++ src-cljs/com/oakmac/chessboard2/util/dom.cljs
@@ -7,6 +7,9 @@
 (defn query-select [q]
   (.querySelector js/document q))
 
+(defn query-select-all [q]
+  (.querySelectorAll js/document q))
+
 (defn get-element
   "does it's best to grab a native DOM element from it's argument
   arg can be either:
@@ -27,6 +30,9 @@
   [el]
   (let [js-box (.getBoundingClientRect el)]
     {:height (gobj/get js-box "height")
+     :left (gobj/get js-box "left")
+     ; :right (gobj/get js-box "right")
+     :top (gobj/get js-box "top")
      :width (gobj/get js-box "width")}))
 
 (defn get-width
