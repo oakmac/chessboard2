fbaaa1c34b44ad301c208195848ec04401dc26f7
Chris Oakman <chris@oakmac.com>
2022-10-24 19:30:16 -0500

    minor tweaks to arrow CSS

--- src-cljs/com/oakmac/chessboard2/util/arrows.cljs
+++ src-cljs/com/oakmac/chessboard2/util/arrows.cljs
@@ -6,15 +6,14 @@
 (defn position
   "Returns a Map of Arrow positioning information"
   [{:keys [board-width end orientation size start] :as _arrow-config}]
-  (let [
-        {start-x :center-left-pct
+  (let [{start-x :center-left-pct
          start-y :center-top-pct} (square->dimensions start board-width orientation)
         {end-x :center-left-pct
          end-y :center-top-pct} (square->dimensions end board-width orientation)
         dx (- end-x start-x)
         dy (- end-y start-y)
         ;; FIXME allow for variable size boards, ie: 6x4 square board
-        square-width 12.5 ;1/8
+        square-width 12.5 ;; 1/8
         head-height (* square-width size)
         base-offset-pct-of-board (/ head-height 6)
         head-width-pct-of-board (* head-height 0.8)
@@ -29,9 +28,8 @@
       {:angle angle
        :arrow-height head-height
        :arrow-width arrow-width
-
+       :base-offset base-offset
        :head-width head-width
        :line-width line-width
        :start-x start-x
-       :start-y start-y
-       :base-offset base-offset}))
+       :start-y start-y}))
