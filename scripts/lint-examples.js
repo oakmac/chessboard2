495ead6a0bfcd1f91df11dc27efc0992600a5152
Aubrey Snider <aubrey@aurmer.com>
2022-10-25 08:22:14 -0500

    finalize lint-examples script

--- scripts/lint-examples.js
+++ scripts/lint-examples.js
@@ -11,25 +11,57 @@ const fs = require('fs-plus')
 const kidif = require('kidif')
 const path = require('path')
 
-fs.removeSync('tmp')
-fs.makeTreeSync('tmp')
+fs.removeSync('tmp-linting')
+fs.makeTreeSync('tmp-linting')
 
 // grab the examples
-const examplesArr = kidif('examples/*.example')
+const examplesArr = kidif('examples/*.example',{camelCaseTitles: false})
 
 // sanity check the examples
 assert(examplesArr, 'Could not load the Example files')
 assert(examplesArr.length > 1, 'Zero examples loaded')
 
+// create temporary js files for linting
 examplesArr.forEach((example) => {
-  fs.writeFileSync(path.join('tmp', example.id + '.js'), example.js)
+  const tmpPath = path.join('tmp-linting', example.id + '.js')
+  fs.writeFileSync(tmpPath, example.JS)
 })
 
-const output = childProcess.execSync('npx standard --fix tmp/', {
-  cwd: path.join(__dirname, '..'),
-  encoding: 'utf8'
+// run linter on temporary folder
+// standard throws errors on non-fixable issues, so log the error
+try{
+  const output = childProcess.execSync(`npx standard --global attachEvent --global Chess --global Chessboard --global Chessboard2 --fix tmp-linting/`, {
+    cwd: path.join(__dirname, '..'),
+    encoding: 'utf8'
+  })
+  console.log(output.stdOut)
+} catch (err) {
+  console.error(err)
+}
+
+// loop over all examples, match up lintedJS for each, and re-write the example
+fs.readdirSync('./examples/').forEach( (file) => {
+  const originalExamplesArr = kidif(`examples/${file}`,{camelCaseTitles: false})
+  const originalExample = originalExamplesArr[0]
+  
+  if(originalExamplesArr.length === 1 && originalExample.id && originalExample.JS) {
+    const lintedJS = fs.readFileSync(`tmp-linting/${originalExample.id}.js`).toString()
+    assert(lintedJS.length > 0)
+
+    let outputString = ''
+
+    const lastKeyIndex = Object.keys(originalExample).length - 1
+    Object.entries(originalExample).forEach(([key, originalValue],idx) => {
+      outputString += `===== ${key}\n` +
+                      `${key === "JS" ? lintedJS : originalValue}`
+      if(idx < lastKeyIndex) {
+        outputString += '\n\n'
+      }
+    })
+
+    fs.writeFileSync(`./examples/${file}`,outputString)
+  }
 })
 
-console.log(output.stdout)
-
-fs.removeSync('tmp')
+// delete temporary linting directory
+fs.removeSync('tmp-linting')
